e1203c3a338acd54f7f314a6806d177a93492864
Martin Whitman <martin.whitman.1776@gmail.com>
2021-05-15 23:20:31 -0500

    Add project files.

---
+++ MvvmTestApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MvvmTestApp.Models;
+using MvvmTestApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MvvmTestApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
